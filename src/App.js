04c3057d5056f9a5d372a0239dc4fdc5fccaac2b
Arnab Chakraborty <arnab2018c@gmail.com>
2021-05-26 15:57:33 +0530

    first_push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Chrome from"./Chrome";
+
+function app () {
+  return (
+      <div>
+           <Chrome/>
+      </div>
+
+
+    
+  );
+}
+export default app;
+
---
+++ src/Chrome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Chrome () {
+  return (
+
+    <h1 className = "app"> 
+      Hello World
+    </h1>
+    
+  );
+}
+export default Chrome;
